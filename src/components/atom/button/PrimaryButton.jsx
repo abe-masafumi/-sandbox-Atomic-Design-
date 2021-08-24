c59826629468b8b23dd0618a04ddd419222cd1a7
abe-masafumi <83285963+abe-masafumi@users.noreply.github.com>
2021-08-25 01:22:47 +0900

    Initial commit

---
+++ src/components/atom/button/PrimaryButton.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const PrimaryButton = (props) => {
+  const { children } = props;
+  return <button>{children}</button>;
+};
+
+const SButton = styled.button`
+  background-color: #40514e;
+  color: #fff;
+  padding: 6px 24px;
+  border: none;
+  outline: none;
+  &:hover {
+    cursor: pointer;
+  }
+`;
